23da3613c720b14ad0fb91b27503b81635dd9411
王璐4号 <wanglu4@37.com>
2024-03-18 16:08:51 +0800

    init

---
+++ Text.py
@@ -0,0 +1,48 @@
+'''
+@Project ：COMFYUI-form 
+@File    ：Text.py
+@Author  ：Deer
+@Email   ：wanglu4@37.com
+@Date    ：2024/3/18 16:06 
+'''
+'''
+@Project ：ComfyUI-I2VGEN_XL 
+@File    ：I2VGenXL.py
+@Author  ：Deer
+@Email   ：wanglu4@37.com
+@Date    ：2024/3/11 15:19 
+'''
+import os
+import cv2
+import torch
+import numpy as np
+import folder_paths
+
+current_directory = os.path.dirname(os.path.abspath(__file__))
+
+class Text_Showcase:
+    def __init__(self):
+        pass
+
+    @classmethod
+    def INPUT_TYPES(cls):
+        return {
+            "required": {
+                "TXT": ("STRING", ),
+            }
+        }
+
+    RETURN_TYPES = ("MODEL",)
+    RETURN_NAMES = ("pipe",)
+    FUNCTION = "show txt"
+    CATEGORY = "Deer"
+
+
+
+NODE_CLASS_MAPPINGS = {
+    "Text_Showcase": Text_Showcase,
+}
+
+NODE_DISPLAY_NAME_MAPPINGS = {
+    "Text_Showcase": "📽Text_Showcase",
+}
